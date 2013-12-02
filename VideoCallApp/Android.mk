833eb0bf56134377596a280f0010794d13a2be1d
madhu.p <madhu.p@borqs.com>
2013-12-02 16:47:54 +0530

    VideoCall App

---
+++ VideoCallApp/Android.mk
@@ -0,0 +1,29 @@
+LOCAL_PATH:= $(call my-dir)
+VT_TOP:=$(LOCAL_PATH)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+
+LOCAL_PACKAGE_NAME := VideoCall 
+LOCAL_CERTIFICATE := platform
+
+# This will install the file in /system/vendor/ChinaUnicom
+LOCAL_MODULE_PATH := $(TARGET_OUT)/vendor/ChinaUnicom/system/app
+
+#LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := VideoCallApi:jar/javalib.jar
+
+#LOCAL_JAVA_LIBRARIES := VideoCallApi
+
+LOCAL_PROGUARD_FLAGS := -include $(LOCAL_PATH)/proguard.flags
+LOCAL_JAVA_LIBRARIES := telephony-common telephony-msim videocallapi
+
+OMS_RESOURCES_LIBRARIES = true
+
+include $(BUILD_PACKAGE)
+#include $(BUILD_MULTI_PREBUILT)
+
+# include all tests and jni libraries
+include $(call all-makefiles-under,$(LOCAL_PATH))
+
